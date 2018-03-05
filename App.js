0650e87d576aa806cca339720a1da8d0e7bf4c20
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-03-05 23:32:19 +0900

    Implement mock of Login page

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { TextInput, ScrollView } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>This is the first time to build x platform app!</Text>
+        <View style={{ flex: 1, alignItems: 'flex-start', top: 30, left: 10 }}>
+          <Button
+            title="←"
+            onPress={() => Alert.alert('Back to top page!')}
+            color="white"
+          />
+        </View>
+        <View style={{flex: 1, left: 20}}>
+          <Text
+            style={{ color: 'white', fontSize: 30}}
+          >
+            ログイン
+          </Text>
+        </View>
+        <View style={{flex: 2, left: 20}}>
+          <View style={{flex: 1,}}>
+            <Text
+              style={{ color: 'white', marginBottom: 10}}
+            >
+              Email
+            </Text>
+            <TextInput
+              style={{ color: 'white', borderBottomColor: 'white', borderBottomWidth: 1, width: '85%', paddingBottom: 10}}
+              placeholder="Please enter your email address."
+              onChangeText={(email) => this.setState({email})}
+            />
+          </View>
+          <View style={{flex: 1,}}>
+            <Text
+              style={{ color: 'white', marginBottom: 10}}
+            >
+              Password
+            </Text>
+            <TextInput
+              style={{ color: 'white', borderBottomColor: 'white', borderBottomWidth: 1, width: '85%', paddingBottom: 10}}
+              placeholder="Please enter your email address."
+              onChangeText={(password) => this.setState({password})}
+            />
+          </View>
+        </View>
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+          <Button
+            title="ログイン"
+            color="white"
+            onPress={() => Alert.alert('Login with ' + this.state.email + ' & ' + this.state.password)}
+          />
+          <Button
+            title="Forgot password?"
+            color="white"
+            onPress={() => Alert.alert('Go to reset password page!')}
+          />
+        </View>
       </View>
     );
   }
@@ -17,8 +71,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'powderblue'
   },
+  textInput: {
+    width: 100,
+    height: 40,
+  },
+  scrolItem: {
+    fontSize: 40
+  }
 });
